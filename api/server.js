ff028f2e2a5708d106c06806448d6b488808af34
Hayman Guddanti <hayman.guddanti@nagarro.com>
2025-01-16 11:38:51 +0530

    inital

--- api/server.js
+++ api/server.js
@@ -1,35 +1,31 @@
-const jsonServer = require("json-server");
-const cors = require('cors'); // Import the cors package
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-const server = jsonServer.create();
+const server = jsonServer.create()
 
 // Uncomment to allow write operations
-const fs = require("fs");
-const path = require("path");
-const filePath = path.join("db.json");
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data);
-const router = jsonServer.router(db);
+ const fs = require('fs')
+ const path = require('path')
+ const filePath = path.join('db.json')
+ const data = fs.readFileSync(filePath, "utf-8");
+ const db = JSON.parse(data);
+ const router = jsonServer.router(db)
 
-const middlewares = jsonServer.defaults();
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
 
-// Enable CORS for all origins or for a specific domain
-server.use(cors());  // This will allow all origins by default
+const middlewares = jsonServer.defaults()
 
+server.use(middlewares)
 // Add this before server.use(router)
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-    "/blog/:resource/:id/show": "/:resource/:id",
-  })
-);
-
-server.use(middlewares);  // Use default json-server middlewares (logging, static, etc.)
-server.use(router);
-
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
+server.use(router)
 server.listen(3000, () => {
-  console.log("JSON Server is running");
-});
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
-module.exports = server;
+module.exports = server
